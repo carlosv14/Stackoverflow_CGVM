cffd5781ff084a469550815705e10b8e6152091a
Carlos Varela <carlos.varela@unitec.edu>
2015-03-25 22:37:45 -0600

    cambios

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using AutoMapper;
+using Newtonsoft.Json;
 using Stackoverflow_CGVM.Data;
 using Stackoverflow_CGVM.Domain.Entities;
 using Stackoverflow_CGVM.Domain.Services;
@@ -197,8 +199,53 @@ namespace WebApplication1.Controllers
             return View(modelo);
         }
 
-       
+        [HttpPost]
+        public ActionResult ValidateCaptcha()
+        {
+            var response = Request["g-recaptcha-response"];
+            //secret that was generated in key value pair
+            const string secret = "6LfdFAQTAAAAAKyvL-t0xljfIHEOMVlQ_aKtoKe6";
 
-      
+            var client = new WebClient();
+            var reply =
+                client.DownloadString(
+                    string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secret, response));
+
+            var captchaResponse = JsonConvert.DeserializeObject<CaptchaResponse>(reply);
+
+            //when response is false check for the error message 
+            if (!captchaResponse.Success)
+            {
+                if (captchaResponse.ErrorCodes.Count <= 0) return View();
+
+                var error = captchaResponse.ErrorCodes[0].ToLower();
+                switch (error)
+                {
+                    case ("missing-input-secret"):
+                        ViewBag.Message = "The secret parameter is missing.";
+                        break;
+                    case ("invalid-input-secret"):
+                        ViewBag.Message = "The secret parameter is invalid or malformed.";
+                        break;
+
+                    case ("missing-input-response"):
+                        ViewBag.Message = "The response parameter is missing.";
+                        break;
+                    case ("invalid-input-response"):
+                        ViewBag.Message = "The response parameter is invalid or malformed.";
+                        break;
+
+                    default:
+                        ViewBag.Message = "Error occured. Please try again";
+                        break;
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Valid";
+            }
+
+            return View();
+        }
     }
 }
